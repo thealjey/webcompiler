423522a7d67bc5a72783bf246000c71d225f3247
Eugene Kuzmenko <aljey1972@gmail.com>
2016-01-08 14:09:23 +0200

    [fixed] fixed minor spacing issue in jsdocPlugin

--- config/jsdocPlugin.js
+++ config/jsdocPlugin.js
@@ -8,7 +8,7 @@ exports.handlers = {
   beforeParse: function (e) {
     if (extension === extname(e.filename)) {
       comments = e.source.match(commentPattern);
-      e.source = comments ? e.source.split(commentPattern).reduce(function(result, source, i) {
+      e.source = comments ? e.source.split(commentPattern).reduce(function (result, source, i) {
         return result + source.replace(notNewLinePattern, '') + comments[i];
       }, '') : e.source.replace(notNewLinePattern, '');
     }
